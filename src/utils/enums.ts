41d47d8a2f58e135782c0496b51e316172f12475
minhglhf <minhtq30102000@gmail.com>
2021-10-05 11:44:42 +0700

    create groups, get list groups

--- src/utils/enums.ts
+++ src/utils/enums.ts
@@ -2,3 +2,13 @@ export enum Mail {
   Confirmation = 'confirmation',
   Resetpassword = 'resetpassword',
 }
+
+export enum Privacy {
+  Public = 'public',
+  Private = 'private'
+}
+
+export enum GroupMemberRole {
+  Admin = 'admin',
+  NormalUser = 'normalUser'
+}
