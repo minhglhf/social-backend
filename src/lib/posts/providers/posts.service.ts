79338ffaccb6be745cc824c55a6cea9cdac16427
minhglhf <minhtq30102000@gmail.com>
2021-12-19 23:47:20 +0700

    fix search ouput

--- src/lib/posts/providers/posts.service.ts
+++ src/lib/posts/providers/posts.service.ts
@@ -276,6 +276,7 @@ export class PostsService {
             .find({
               hashtags: hashtagsArr[0],
             })
+            .populate('user', ['avatar', 'displayName'])
             .sort([['date', 1]])
             .select(['-__v'])
             .skip(skip)
@@ -294,6 +295,7 @@ export class PostsService {
           .find({
             hashtags: { $all: hashtagsArr },
           })
+          .populate('user', ['avatar', 'displayName'])
           .sort([['date', 1]])
           .select(['-__v'])
           .skip(skip)
